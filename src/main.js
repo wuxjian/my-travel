7f53fbed6e435ef065e8dd455e15c62a9cede30f
wuxjian <wu_xjian@163.com>
2020-08-22 17:15:21 +0800

    头部swiper完成

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import fastclick from 'fastclick'
+import VueAwesomeSwiper  from 'vue-awesome-swiper'
 
 //reset.css
 import './assets/reset.css'
 import './assets/border.css'
 //icon-font
 import './assets/iconfont.css'
+//swiper
+import 'swiper/swiper-bundle.css'
 
 Vue.config.productionTip = false
 //使用fastclick
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions" >
+      <swiper-slide v-for="item of dataList" :key="item.id">
+        <img class="swiper-image" :src="item.imageUrl" alt="">
+      </swiper-slide>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "HeaderSwiper",
+    data(){
+      return {
+        dataList: [
+          {
+            id: '001',
+            imageUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/b8c4527c41649814cc4cf86880abba54.jpeg',
+          },
+          {
+            id: '002',
+            imageUrl: 'https://imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg',
+          }
+        ],
+        swiperOptions: {
+          pagination: '.swiper-pagination',
+          loop: true
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 32.8%
+    .swiper-image
+      width 100%
+</style>
